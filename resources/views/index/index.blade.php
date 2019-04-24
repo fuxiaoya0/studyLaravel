acc85a12a683d6b7d974dad4a657ff0e697e14b6
符肖亚 <fxy@shtruecolor.com>
2019-04-24 16:33:49 +0800

    ,

---
+++ resources/views/index/index.blade.php
@@ -0,0 +1,21 @@
+@extends("layout.app")
+@section("title")
+    {{$title}}
+@endsection
+@section('body')
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-3">
+
+            </div>
+            <div class="col-sm-9">
+
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    <script type="javascript" src="./bootstrap-3.3.7/js/bootstrap.js"></script>
+    <link rel="stylesheet" href="./bootstrap-3.3.7/css/bootstrap.css">
+</head>
+<body>
+<div class="jumbotron">
+    <div class="container">
+        <h1>Hello,world!</h1>
+    </div>
+</div>
+@section("body")
+    这里是body
+@show
+</body>
+</html>
